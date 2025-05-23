ee36869ea730addebe9197f8bbfd3e190e3f0bad
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-05-23 18:11:45 +0200

    [ADD] monitor_radiation_levels.patch

--- rendu/monitor_radiation_levels.c
+++ rendu/monitor_radiation_levels.c
@@ -7,24 +7,22 @@
 
 #include <stdio.h>
 #include <string.h>
-void secret_function ()
-	{
-		printf (
-				"{The stone isn't in the pocket anymore ...}\n");
-	}
-void monitor_radiation_levels ()
-	{
-		char    buffer[10];
-		void (* function_ptr) () = NULL;
-		printf ("Enter radiation levels: ");
-		gets (buffer);
-		printf ("Radiation Levels: %s\n" ,buffer);
-		if (function_ptr)
-			{
-				function_ptr();
-			} else
-			{
-				printf ("Function Pointer: %p\n"
-						,( void * ) function_ptr);
-			}
-	}
+
+void secret_function(void)
+{
+    printf("{The stone isn't in the pocket anymore ...}\n");
+}
+
+void monitor_radiation_levels(void)
+{
+	char buffer[10];
+	void (*function_ptr)(void) = NULL;
+
+	printf("Enter radiation levels: ");
+	fgets(buffer, 10, stdin);
+	printf("Radiation Levels: %s\n", buffer);
+	if (function_ptr)
+	    function_ptr();
+	else
+        printf("Function Pointer: %p\n", (void *)function_ptr);
+}
