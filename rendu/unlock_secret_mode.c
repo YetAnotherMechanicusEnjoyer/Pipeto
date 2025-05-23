66592d84ef0b50c4de52ebb6a6a3c5fad7468b68
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-05-23 18:20:06 +0200

    [ADD] unlock_secret_mode.patch

--- rendu/unlock_secret_mode.c
+++ rendu/unlock_secret_mode.c
@@ -2,7 +2,7 @@
 ** EPITECH PROJECT, 2024
 ** Pipeto
 ** File description:
-** unlock_secret_mode
+** unlock_secret_mode - SECURED VERSION
 */
 
 #include <stdio.h>
@@ -10,12 +10,12 @@
 #include <string.h>
 #include "pipeto.h"
 
-void unlock_secret_mode()
+void unlock_secret_mode(void)
 {
-    if (IS_ADMIN) {
-        printf("Secret mode unlocked! Welcome, admin.\n");
-        printf("{ADMIN4242}\n");
-    } else {
+    if (!IS_ADMIN) {
         printf("Access denied. You do not have the required privileges.\n");
+        return;
     }
+    printf("Secret mode unlocked! Welcome, admin.\n");
+    printf("{ADMIN4242}\n");
 }
